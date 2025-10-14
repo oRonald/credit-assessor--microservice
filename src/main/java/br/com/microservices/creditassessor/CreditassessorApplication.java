621cf2efdc53f4a4bab2cf4f4a1ba745ed045d29
oRonold <or040918@gmail.com>
2025-10-13 23:51:28 -0300

    setup credit assessor microservice

---
+++ src/main/java/br/com/microservices/creditassessor/CreditassessorApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservices.creditassessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditassessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditassessorApplication.class, args);
+	}
+
+}
